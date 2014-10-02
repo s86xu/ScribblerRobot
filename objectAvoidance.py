bfbc01cbcf8efb66b58bc986eadec535852143f9
StudyOnly <s86xu@uwaterloo.ca>
2014-10-02 14:16:02 -0400

    Obsticle Avoid Update 0.2

--- objectAvoidance.py
+++ objectAvoidance.py
@@ -1,9 +1,10 @@
-#Obstacle Avoidance
+# Obstacle Avoidance
+
 from myro import *
 initialize("/dev/tty.IPRE6-196107-DevB")
 
-cruiseSpeed = 0.6
-turnSpeed = 0.5
+final cruiseSpeed = 0.6
+final time = 0.5
 
 def leftRotate():
     rotate(0.8)
@@ -15,6 +16,10 @@ def rightRotate():
     wait(0.95)
     stop()
 
+def cruise():
+    global cruiseSpeed, time
+    move(cruiseSpeed, time)
+
 def checkObstacle():
     L,C,R = getObstacle()
     if L > 1000 and C > 1000 and R > 1000:
@@ -22,21 +27,50 @@ def checkObstacle():
     else:
         return False
 
+def stageOne():
+    if checkObstacle():
+        leftRotate()
+        cruise()
+	rotateRight()
+	return True
+    else:
+	return False
+
+def stageTwo():
+    rightRotate()
+    if checkObstacle():
+	leftRotate()
+	cruise()
+	return True
+    else:
+	return False
+
+
 
 def main():
 
-while True:
+    switch = False
     
-            if checkObstacle():
-                leftRotate()
-                move(cruiseSpeed,0)
-                wait(0.5)
-                rotateRight()
-            elif #Some condition here:
-                move(cruiseSpeed,0)
-                rotateRight()
-                if checkObstacle():
-                    rotateLeft()
+    while(!checkObstacle()):
+	cruise()
 
-        
-                
+    switch = True
+
+    while(switch):
+	switch = stageOne()
+
+    switch = True
+    cruise()
+	
+    while(switch):
+	switch = stageTwo()
+
+    switch = True
+
+    while(switch):
+	cruise()
+
+
+
+
+
