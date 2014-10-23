efef94201c625f1a04202f1b122802381ee27fda
s86xu <s86xu@uwaterloo.ca>
2014-10-23 12:34:37 -0400

    added Diagonal

--- objectAvoidance.py
+++ objectAvoidance.py
@@ -14,20 +14,26 @@ def rightRotate(): # Rotate 90 to the right
     wait(0.95)
     stop()
 
-def cruise(): # movevent forward for a fixed distance, change the variable.
-    cruiseSpeed = 1
-    time = 0.7 #CHANGE MADE
-    move(cruiseSpeed, 0)
+def cruise(speed, time):
+    move(speed, 0)
     wait(time)
     stop()
 
+def cruise(): # movevent forward for a fixed distance, change the variable.
+    cruiseSpeed = 1
+    time = 0.7
+    cruise(cruiseSpeed, time)
+
 def checkObstacle(): # check obstacle function, main func. Perhaps need tweaking
     L,C,R = getObstacle()
-    if L > 800 and C > 800 and R > 800: #CHANGE
+    if L > 800 or C > 800 or R > 800:
         return True
     else:
         return False
 
+
+
+# //-----------------------------------------------------------------------------
 def stageOne(): # check for object avoid while it is in first stage.
     if checkObstacle():
         leftRotate()
@@ -43,39 +49,36 @@ def stageTwo():  # check for object while in second stage
         leftRotate()
         cruise()
         return True
-    else:
+    else:OB
         return False
 
 
-
-def body():
+# //----------------------------------------------------------------------------------
+def Straight():
 
     switch = False # initialize the switch
     count = -1 # check if this should be 0 or -1
 
-    while(not checkObstacle()): # while there is no obsticle infront, move forward
-        move(1,0)
-    stop()
-    print "stopped, obst dec"
+    print "Initial Trigger Object Detected!"
 
     switch = True # initialized the switch
 
     while(switch): # bot stuck in first stage, keep track of distance traveled
         count += 1
         switch = stageOne()# switch should be in OFF for this loop to break.
-#Changes START
+
     leftRotate()
-    move(1,0)
-    wait(0.5)
-    stop()
+    cruise(1, 0.5)
+
     rightRotate()
-    move(1,0)
-    wait(0.5)
-    stop()
-#Changes END
+    cruise(1,0.5)
+
     switch = True # reset the switch
-    cruise()
-	
+    cruis()
+
+    print "Stage One Complete! Moving on to Stage Two."
+
+    # //---------------------------------------------------------------------------------
     while(switch): # bot stuck in stage 2
         switch = stageTwo() # switch should be off for this loop to break.
     leftRotate()
@@ -84,18 +87,76 @@ def body():
     stop()
     rightRotate()
 
-    print count
+    print "Stage Two Complete! Moving back to the Origional Rotation"
+
+    # //----------------------------------------------------------------------------------
     for i in range(0, count): # Check value for 1 or 0, //return to the regular distance
         cruise()
 
     leftRotate() # Return to the origional facing position
 
+    # //----------------------------------------------------------------------------------
     cruise() # End of the body() function
 
+# //---------------------------------------------------------------------------------
+
+def leftOne():
+    leftRotate()
+    cruise()
+    rightRotate()
+    cruise()
+
+def leftTwo():
+    rightRotate()
+    cruise()
+    leftRotate()
+    cruise()
+
+def rightOne():
+    rightRotate()
+    cruise()
+    leftRotate()
+    cruise()
+
+def rightTwo():
+    leftRotate()
+    cruise()
+    rightRotate()
+    cruise()
+
+def Diag(case):
+    if case == 1: # Object on the Right side.
+
+    elif case == 2: # Object on the Left side.
+
+    #else:
+
+# //----------------------------------------------------------------------------------
 def main():
-    count = 2 # the number of obsticle that it need to pass through before it shutdown.
-        #for i in range(0, count):
-    body()
+
+    while(not checkObstacle()): # while there is no obsticle in front, move forward
+        move(1,0)
+    stop()
+
+
+    leftRotate()
+    bolLeft = checkObstacle()
+
+    rightRotate()
+    rightRotate()
+    bolRight = checkObstacle()
+
+    leftRotate()
+
+    if bolRight == False and bolLeft == False:
+        Straight()
+    elif bolRight:
+        Diag(1)
+    elif bolLeft:
+        Diag(2)
+
+
+# //-----------------------------------------------------------------------------------
 
 main() # The start of the program.
 
