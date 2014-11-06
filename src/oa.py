fa88b62e7c89e1d8807d7c81bd56ddc0c89f0eaa
s86xu <s86xu@uwaterloo.ca>
2014-11-06 11:16:43 -0500

    combine all OA

---
+++ src/oa.py
@@ -0,0 +1,52 @@
+from basicFunctions import *
+
+def stageOne(case):
+    if case == 1: # Straight()
+        if flukeCheck():
+            turnLeft()
+            mvForward()
+            turnRight()
+            print "Case 1: Stage 1 Running ..."
+            return True
+        else:
+            print "Case 1: Stage 1 End."
+            return False
+        
+    elif case == 2: # diagonal()
+
+    elif case == 3: # diagonal()
+
+
+
+def stageTwo(case):
+    if case == 1: # Straight()
+        turnRight()
+        if flukeCheck():
+            turnLeft()
+            mvForward()
+            print "Case 1: Stage 2 Running ..."
+            return True
+        else:
+            print "Case 1: Stage 2 End."
+            return False
+
+    elif case == 2: # diagonal()
+
+
+
+    elif case == 3: # diagonal()
+
+
+def objectAvoid():
+
+    count = 0
+    switch = True
+    
+    # Check the 3 cases:
+
+    # The Case # and result:
+    case = 1
+
+    # dependent on Case 1 or Two or 3:
+    
+    
